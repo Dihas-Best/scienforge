c4806289418d6f1a277e1dd7c083a37b5f8d36b9
Dihas-Best <dihas.nanayakkara@gmail.com>
2026-09-04 06:24:03 +0530

    Remove duplicate Google Ads script tag

--- scienforge/src/app/about/page.tsx
+++ scienforge/src/app/about/page.tsx
@@ -42,6 +42,7 @@ export default function About() {
         </p>
       </div>
     </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9110644120668325"
+     crossorigin="anonymous"></script>
   );
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9110644120668325"
-     crossorigin="anonymous"></script>}
+}
