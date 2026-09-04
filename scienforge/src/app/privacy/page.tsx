ee898961a5be3c483c3c3565182e5e13c34908d9
Dihas-Best <dihas.nanayakkara@gmail.com>
2026-09-04 06:23:08 +0530

    Add Google Ads script to privacy page

--- scienforge/src/app/privacy/page.tsx
+++ scienforge/src/app/privacy/page.tsx
@@ -62,5 +62,7 @@ export default function Privacy() {
         </p>
       </div>
     </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9110644120668325"
+     crossorigin="anonymous"></script>
   );
 }
