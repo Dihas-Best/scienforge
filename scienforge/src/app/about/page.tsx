c1ce93c9892e5580c028902b6a3ea7e5dcbeabc3
Dihas-Best <dihas.nanayakkara@gmail.com>
2026-09-04 06:24:45 +0530

    Remove ads script from about page
    Removed Google Ads script from the about page.

--- scienforge/src/app/about/page.tsx
+++ scienforge/src/app/about/page.tsx
@@ -42,7 +42,6 @@ export default function About() {
         </p>
       </div>
     </div>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9110644120668325"
-     crossorigin="anonymous"></script>
+    
   );
 }
